1b215688cd22c135b48ab5af12aa984cf5e50351
Mohd Saquib <mohdsaquib66083@gmail.com>
2026-08-02 18:01:45 +0530

    Update Changes

--- db.py
+++ db.py
@@ -2,15 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
-    DATABASE_URL = os.getenv("DATABASE_URL"),
+   DATABASE_URL,
     pool_pre_ping=True,
-    connect_args={
-        "ssl":{
-            "ca": r"C:/Users/hp/Downloads/isrgrootx1.pem"
-        }
-    }
+    #connect_args={
+     #   "ssl":{
+         #   "ca": r"C:/Users/hp/Downloads/isrgrootx1.pem"
+     #   }
+    #}
 )
 
 SessionLocal = sessionmaker(bind=engine)
