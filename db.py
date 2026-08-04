16f6be25c02cc9280e21f9dba9e52f90ae85d104
Mohd Saquib <mohdsaquib66083@gmail.com>
2026-08-04 13:24:21 +0530

    Making changes

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-import os
+import os,ssl
 
 load_dotenv()
 
@@ -10,10 +10,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(
    DATABASE_URL,
     pool_pre_ping=True,
-    #connect_args={
+    connect_args={
      #   "ssl":{
          #   "ca": r"C:/Users/hp/Downloads/isrgrootx1.pem"
-     #   }
+          "ssl": ssl.create_default_context()
+       }
     #}
 )
 
